2cdeb2fd4144ba8515eac30d997b4c6cd94f5c30
sysleg <zlou.sysleg@gmail.com>
2017-04-23 18:28:17 +0300

    Реализовано воспроизведение звуков по кликам на кнопки

--- app/src/main/java/com/bignerdranch/android/beatbox/BeatBox.java
+++ app/src/main/java/com/bignerdranch/android/beatbox/BeatBox.java
@@ -1,7 +1,10 @@
 package com.bignerdranch.android.beatbox;
 
 import android.content.Context;
+import android.content.res.AssetFileDescriptor;
 import android.content.res.AssetManager;
+import android.media.AudioManager;
+import android.media.SoundPool;
 import android.util.Log;
 
 import java.io.IOException;
@@ -13,10 +16,14 @@ import java.util.List;
  */
 
 public class BeatBox {
-    private static final String TAG = "BeatBox";
-    private static final String SOUNDS_FOLDER = "sample_sounds";
+
     private AssetManager mAssets;
     private List<Sound> mSounds = new ArrayList<>();
+    private SoundPool mSoundPool;
+
+    private static final String TAG = "BeatBox";
+    private static final String SOUNDS_FOLDER = "sample_sounds";
+    private static final int MAX_SOUNDS =5;
 
     public List<Sound> getSounds() {
         return mSounds;
@@ -24,8 +31,11 @@ public class BeatBox {
 
     public BeatBox(Context context){
         mAssets = context.getAssets();
+        mSoundPool = new SoundPool(MAX_SOUNDS, AudioManager.STREAM_MUSIC, 0);
         loadSounds();
     }
+
+
 
     private void loadSounds(){
         String[] soundNames;
@@ -39,9 +49,31 @@ public class BeatBox {
 
         for(String filename : soundNames)
         {
-            String assetPath = SOUNDS_FOLDER+"/"+filename;
-            Sound sound = new Sound(assetPath);
-            mSounds.add(sound);
+            try{
+                String assetPath = SOUNDS_FOLDER+"/"+filename;
+                Sound sound = new Sound(assetPath);
+                load(sound);
+                mSounds.add(sound);
+            } catch (IOException ioe){
+                Log.e(TAG, "Could not load sound " + filename, ioe);
+            }
         }
     }
+
+    private void load(Sound sound) throws IOException{
+        AssetFileDescriptor fileDescriptor = mAssets.openFd(sound.getAssetPath());
+        int soundId = mSoundPool.load(fileDescriptor,1);
+        sound.setSoundId(soundId);
+    }
+
+    public void play(Sound sound){
+        Integer soundId = sound.getSoundId();
+        if(soundId == null)
+        {
+            return;
+        }
+        mSoundPool.play(soundId,1.0f, 1.0f, 1, 0, 1.0f);
+    }
+
+    public void release(){ mSoundPool.release(); }
 }
--- app/src/main/java/com/bignerdranch/android/beatbox/BeatBoxFragment.java
+++ app/src/main/java/com/bignerdranch/android/beatbox/BeatBoxFragment.java
@@ -35,24 +35,38 @@ public class BeatBoxFragment extends Fragment {
     }
 
     @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mBeatBox.release();
+    }
+
+    @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        setRetainInstance(true);
         mBeatBox = new BeatBox(getActivity());
     }
 
-    private class SoundHolder extends RecyclerView.ViewHolder{
+    private class SoundHolder
+            extends RecyclerView.ViewHolder
+            implements View.OnClickListener
+    {
         private Button mButton;
         private Sound mSound;
 
         public SoundHolder(LayoutInflater inflater, ViewGroup container) {
             super(inflater.inflate(R.layout.list_item_sound,container,false));
-
             mButton = (Button)itemView.findViewById(R.id.list_item_sound_button);
+            mButton.setOnClickListener(this);
         }
         public void bindSound(Sound sound){
             mSound = sound;
             mButton.setText(mSound.getName());
+        }
+
+        @Override
+        public void onClick(View v) {
+            mBeatBox.play(mSound);
         }
     }
     private class SoundAdapter extends RecyclerView.Adapter<SoundHolder>{
